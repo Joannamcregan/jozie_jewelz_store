1c9ff65ded2f6c80c3a8150ba25b767edd36f072
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-02-27 13:02:32 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
                 <img src="<?php echo get_theme_file_uri('/img/her_first_cover_200.jpg'); ?>" alt="In the cover for Her First, a short story by Jozie Jewelz, a blurred image of a young, white man in a t-shirt and baseball cap appears on the screen of a phone, which rests in the manicured hands of a young, white woman." class="book-cover" />
                 <div class="shelf"></div>
             </div>
-            <div class="book-sticker-section">
-                <div class="book-sticker" id="her-first-sticker">
+            <div class="book-flyer-section">
+                <div class="book-flyer" id="her-first-flyer">
                     <p>When the good girl meets the bad guy and neither is quite what the other expected...</p>
                     <p><a>read now</a></p>
                 </div>
@@ -25,6 +25,18 @@
     </div>
     <div id="novels-section" class="book-collection-section">
         <h2>Novels</h2>
+        <div class="book-section">
+            <div class="book-sticker-section">
+                <div class="book-sticker" id="wtaak-sticker">
+                    <p><em>I thought about how love was the curse of hustler, because how could I ever relate to someone who wasn't like me, but how could I ever trust someone who was?</em></p>
+                    <p class="centered-text"><strong>coming soon!</storng></p>
+                </div>
+            </div>
+            <div class="book-shelf-section">
+                <img src="<?php echo get_theme_file_uri('img/wtaak_cover_180.jpg'); ?>" alt="In the cover for What Thieves and Artists Know by Jozie Jewelz, shadows and blue and purple lights drape over a curvy young woman wearing lingerie." class="book-cover" />
+                <div class="shelf"></div>
+            </div>
+        </div>
     </div>
     <div id="mode-selector-wrapper" class="light">
         <div id="mode-selector">
@@ -34,3 +46,6 @@
         </div>
     </div>
 </main>
+<div id="ceiling" aria-hidden="true">
+    <div id="ceiling-light" aria-hidden="true"></div>
+</div>
