9695400d47a71ed4fb1b11b2d1a7c26281a56a6c
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-03-07 03:56:39 -0500

    Add files via upload

---
+++ header-myspace.php
@@ -0,0 +1,20 @@
+<! DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset='<?php bloginfo('charset'); ?>'>
+    <meta name = "viewport" content = "width=device-width", initial-scale=1>
+    <!-- <meta property="og:image" content="<?php echo get_theme_file_uri('/images/screenshot.png'); ?>" />
+    <meta name="twitter:image" content="<?php echo get_theme_file_uri('/images/screenshot.png'); ?>"> -->
+    <title>JozieSpace</title>
+    <?php wp_head(); ?>
+</head>
+<body class="myspace">
+<header>
+    <nav>
+        <a>books</a>
+        <a>blog</a>
+        <a>connect</a>
+    </nav>
+</header>
+</div>
+
