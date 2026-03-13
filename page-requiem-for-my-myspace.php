1458f482aedc166cbf5c2820ad07c3f7be91c87e
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-03-13 11:29:53 -0400

    Add files via upload

--- page-requiem-for-my-myspace.php
+++ page-requiem-for-my-myspace.php
@@ -7,14 +7,14 @@ $diff = $current_date_obj->diff($birth_date_obj);
 $age_years = $diff->y;
 
 ?><main id="myspace-main">
-    <div id="myspace-section-1">
-        <div id="basic-info">
-            <h1>Jozie Jewelz</h1>
-            <img src="<?php echo get_theme_file_uri('/img/myspace_2026.jpg'); ?>" alt="a kind-of young white woman with wavy reddish brown hair and glasses smiles while looking up at the camera" />
-            <p>pronouns: she/her</p>
-            <p><?php echo 'age: ' . $age_years; ?></p>
-            <p>location: Northeast Ohio</p>
-        </div>
+    <div id="basic-info">
+        <h1>Jozie Jewelz</h1>
+        <img src="<?php echo get_theme_file_uri('/img/myspace_2026.jpg'); ?>" alt="a kind-of young white woman with wavy reddish brown hair and glasses smiles while looking up at the camera" />
+        <p>pronouns: she/her</p>
+        <p><?php echo 'age: ' . $age_years; ?></p>
+        <p>location: Northeast Ohio</p>
+    </div>
+    <div id="myspace-section-0">
         <div aria-label="YouTube Video" role="region">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/D1nh3bGrgHM?si=fS-lTLULtDtf3UtR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen role="region"></iframe>
         </div>
@@ -25,15 +25,14 @@ $age_years = $diff->y;
             <p>Instagram: @joziejewelz</p>
             <a href="https://www.glitter-graphics.com" target="_blank" class="glitter-graphic"><img src="http://dl9.glitter-graphics.net/pub/687/687779doa3stdf14.gif" width=101 height=90 border=0></a>
         </div>
-        <img src="<?php echo get_theme_file_uri('/img/sparkle_jozie.gif'); ?>" />
     </div>
     <div id="newsletter-section">
         <script async src="https://eocampaign1.com/form/a7bb2ba6-7f63-11f0-83c1-b7699d22af41.js" data-form="a7bb2ba6-7f63-11f0-83c1-b7699d22af41"></script>
     </div>
+    <a href="https://www.glitterfy.com/" target="_blank" class="glitter-text" id="glitter-jozie-jewelz-writes"><img src="https://img88.glitterfy.com/26072/glitterfy6070904T3971.gif" alt="glittery blue text spells out Jozie Jewelz Writes" border=0 /></a><br />
     <div id="myspace-section-1">
         <div id="top-friends">
             <h2>Jozie's Top 16</h2>
-            <img src=""
         </div>
     </div>
 </main>
