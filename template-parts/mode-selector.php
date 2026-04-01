a709724c1703c2156386821617db0a99fe54bb3d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-04-01 10:00:58 -0400

    Add files via upload

---
+++ template-parts/mode-selector.php
@@ -0,0 +1,8 @@
+<?php echo '<div id="mode-selector-wrapper">
+    <div id="mode-selector">
+        <img src="' . get_theme_file_uri('/img/glasses_80.png') . '" alt="a simple illustration of glasses with black frames" class="selection" id="light-glasses" data-mode="light"/>
+        <img src="' . get_theme_file_uri('/img/sepia_glasses_80.png') . '" alt="a simple illustration of glasses with sepia lenses" class="selection" id="sepia-glasses" data-mode="sepia"/>
+        <img src="' . get_theme_file_uri('/img/dark_glasses_80.png') . '" alt="a simple illustration of sunglasses with dark brown frames" class="selection" id="dark-glasses" data-mode="dark"/>
+    </div>
+</div>'
+?>
