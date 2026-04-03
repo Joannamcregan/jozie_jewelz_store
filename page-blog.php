6b9b805c6407dea9d84e78f17b3e89908dd75fb7
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-04-03 14:18:34 -0400

    Add files via upload

--- page-blog.php
+++ page-blog.php
@@ -1,17 +1,19 @@
 <?php get_header(); 
-    ?><main>
-        <?php $args = array(
-            'post_type'=> 'post',
-            'orderby'    => 'ID',
-            'post_status' => 'publish',
-            'order'    => 'DESC',
-            'posts_per_page' => -1
+    ?><main class="blog-archive">
+        <h1>Blog Posts</h1>
+        <div id="blog-post-flyer-container">
+            <?php $args = array(
+                'post_type'=> 'post',
+                'orderby'    => 'ID',
+                'post_status' => 'publish',
+                'order'    => 'DESC',
+                'posts_per_page' => -1
             );
             $result = new WP_Query( $args );
             if ( $result-> have_posts() ) : ?>
             <?php while ( $result->have_posts() ) : $result->the_post(); ?>
-            <div class="<?php echo 'post-flyer-' . get_the_ID() % 3; ?>">
-                <p><a href='<?php echo get_the_permalink(); ?>'><?php the_title(); ?></a></p>     
+            <div class="<?php echo 'post-flyer post-flyer-' . get_the_ID() % 3; ?>">
+                <h2><a href='<?php echo get_the_permalink(); ?>'><?php the_title(); ?></a></h2>     
                 <p><?php echo get_the_excerpt(); ?></p>
             </div>
             <?php endwhile; ?>
